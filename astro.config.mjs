6a911c13a88623f11da630c5e9138a34d70251a4
Oskar Barcz <oskar@barcz.me>
2025-12-10 22:29:36 +0100

    Configure CI/CD

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://example.com",
